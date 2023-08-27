ac30c4c976f096dd23c0315d98628f22e1a216dc
Ephraaim daviid <Ephraim.daviid@gmail.com>
2023-08-27 20:18:03 +0100

    first commit

---
+++ FIFA/src/Player.js
@@ -0,0 +1,33 @@
+const products =[
+    
+    {
+        image: "/halland.jpg",
+        name: "Halland",
+        age: 22,
+        team: "Manchester city",
+        number: "17",
+        nationality: "British",
+    },
+    {
+        image: "/rashford.png",
+        name: "Rashford",
+        age: 22,
+        team: "Manchester city",
+        number: "9",
+        nationality: "British",
+    }
+    ,
+    {
+        image: "/Rol.jpeg",
+        name: "Rashford",
+        age: 22,
+        team: "Manchester city",
+        number: "7",
+        nationality: "British",
+    }
+    ,
+
+
+]
+
+export default products;
